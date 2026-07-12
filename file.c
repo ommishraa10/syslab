c06e5916eaba30602ee08e31a4e69bc7fe8357c2
ommishraa10 <ommishraa@10gmail.com>
2026-07-12 11:22:14 +0000

    added line count,word count

--- file.c
+++ file.c
@@ -6,6 +6,9 @@ int main(){
     char file_name[100];
     int ch;
     int count=0;
+    int count_line=0;
+    int count_word=0;
+    int in_word=0;
     printf("enter file name or path:");
     scanf("%s", file_name);
 
@@ -13,12 +16,27 @@ int main(){
 
 if(FP==NULL){
     printf("\nfile not found\n");
+return 1;
 }
 else{
     while((ch=fgetc(FP))!=EOF){
         count=count+1;
+      if(ch=='\n'){
+        count_line=count_line+1;
+      }
+      if(ch==' '|| ch=='\n' || ch=='\t'){
+        in_word=0;
+      }else if(in_word==0){
+        in_word=1;
+        count_word=count_word+1;
+      }
     }
-    printf("number of characters in the file: %d", count);
 }
+      
+    
+    printf("\nnumber of characters in the file: %d", count);
+    printf("\nnumber of characters in the file: %d", count_line);
+    printf("\nnumber of characters in the file: %d", count_word);
+
 return 0;
 }
